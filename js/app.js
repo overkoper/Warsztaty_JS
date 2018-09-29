b7919790f769d669f389800db37e85d838490d73
overkoper <piotr.kopczynski7@gmail.com>
2018-09-29 14:48:57 +0200

    Zadanie 2 - chowane przyciski więcej/mniej

--- js/app.js
+++ js/app.js
@@ -10,4 +10,25 @@ var subMenu = document.querySelector('.dropdown');
         subMenu.style.display = 'none';
     })
 
+var buttons = document.querySelectorAll('.read-more') ;
+//    console.log(buttons[0].previousElementSibling);
+    
+    
+    
+    
+function showHide(){
+    var textArea = this.previousElementSibling;
+        if (textArea.style.display === 'none' || textArea.style.display === ''){
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>'
+        }else{
+            textArea.style.display = 'none';
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>'
+        }
+}
+
+//    buttons[0].addEventListener('click', showHide);
+    for (var i=0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', showHide);
+    }
 });
